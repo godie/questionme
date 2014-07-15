bc374dcdf2ef273759ebf304c630a5794bcd83c4
Diego <godie.menodza@gmail.com>
2014-07-15 17:13:34 -0600

    configurado devise y omniauth

--- Gemfile
+++ Gemfile
@@ -22,6 +22,13 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
+gem 'bootstrap-generators', '~> 3.1.1'
+
+gem 'devise'
+gem 'omniauth'
+gem 'omniauth-twitter'
+gem 'omniauth-facebook'
+gem 'mysql2'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+       root to: "home#index"
+       #devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+       match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
